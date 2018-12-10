d9a13305531002dc17a32ebe2ef44f4ee6106578
Daniel Reszka <dannn.r@gmail.com>
2018-12-10 09:28:42 +0100

    fetch & render TagsTree on home page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+const TagsTree = ({ tags }) => <ul>
+  { tags.map(t => 
+    <li>
+      { t.name }
+      { t.tags && <TagsTree tags={t.tags} /> }
+    </li>
+  )}
+</ul>
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { tags: [] }
   }
+
+  componentDidMount = async () => 
+    this.setState(await (await fetch('/api/tags')).json())
+
+  render = () => <div class='App'>
+    <TagsTree tags={ this.state.tags } />
+  </div>
 }
 
-export default App;
+export default App
