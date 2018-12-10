bc77c365a2e7d58a7b758437cd5468b32d168f75
Daniel Reszka <dannn.r@gmail.com>
2018-12-10 01:03:01 +0100

    add new person and tags to insertSeedData

--- server/queries/insertSeedData.js
+++ server/queries/insertSeedData.js
@@ -1,13 +1,36 @@
 const neo = require('../neo4j')
 
-module.exports = () => neo.query(`
-  CREATE 
-    (person:Person { name: $person } ) 
-  -[:HAS_SKILL { weight: 10 }]-> 
-    (tag:Tag {name: $tag}) 
-  -[:CHILD_OF]-> 
-    (tag2:Tag {name: $tag2})
-  RETURN person,tag,tag2
-  `,
-  { tag: 'ReactJS', tag2: 'development', person: 'Tom' }
-)
+
+const createAlice = () => 
+  neo.query(`
+    CREATE 
+      (person:Person { name: $person } ) 
+    -[:HAS_SKILL { weight: 5 }]-> 
+      (tag:Tag {name: $tag}) 
+    -[:CHILD_OF]-> 
+      (tag2:Tag {name: $tag2})
+    -[:CHILD_OF]-> 
+      (tag3:Tag {name: $tag3})
+    RETURN person,tag,tag2,tag3
+    `,
+    { tag: 'SEO', tag2: 'Marketing', tag3: 'Communication', person: 'Alice' }
+  )
+
+const createTom = () => 
+  neo.query(`
+    CREATE 
+      (person:Person { name: $person } ) 
+    -[:HAS_SKILL { weight: 10 }]-> 
+      (tag:Tag {name: $tag}) 
+    -[:CHILD_OF]-> 
+      (tag2:Tag {name: $tag2})
+    RETURN person,tag,tag2
+    `,
+    { tag: 'ReactJS', tag2: 'development', person: 'Tom' }
+  )
+
+module.exports = async () => {
+  const alice = await createAlice()
+  const tom = await createTom()
+  return { alice, tom }
+}
