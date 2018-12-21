7bbfe1efbc4c2a04a0ffa8b860b5483b00f3f8ce
Daniel Reszka <dannn.r@gmail.com>
2018-12-21 23:50:06 +0100

    add basic styles to persons links

--- src/App/PersonsList/PersonsList.js
+++ src/App/PersonsList/PersonsList.js
@@ -1,13 +1,21 @@
 import Link from 'redux-first-router-link'
 import React from 'react'
+import styled from 'styled-components/macro'
 
 import { toPerson } from 'store/routerActions'
+
+const A = styled(Link)`
+  color: rgba(255, 255, 255, 0.5);
+  &:hover {
+    color: rgba(255, 255, 255, 1);
+  }
+`
 
 const PersonsList = ({ persons }) => (
   <ul>
     {persons.map(p => (
       <li key={p.id}>
-        <Link to={toPerson({ id: p.id })}>{p.name}</Link>
+        <A to={toPerson({ id: p.id })}>{p.name}</A>
       </li>
     ))}
   </ul>
